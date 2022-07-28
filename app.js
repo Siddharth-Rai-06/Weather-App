54ae6179455b35d4b09cca3464dd966a3a20caaa
Siddharth_Rai <sidrai0620@gmail.com>
2022-07-28 20:28:20 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express=require("express");
+const app=express();
+const https=require("https");
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+
+
+});
+app.post("/",function(req,res){
+  const query=req.body.cityName;
+  const apiKey="6f3ecce53f8224b26049447df8c1c8c6";
+  const units="metric";
+  const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+units+"&appid="+apiKey;
+  https.get(url,function(response){
+    console.log(response.statusCode);
+    response.on("data",function(data){
+      const weatherData=JSON.parse(data);
+      //console.log(weatherData);
+      const temp=weatherData.main.temp;
+      const desc=weatherData.weather[0].description;
+      const icon=weatherData.weather[0].icon;
+      const imgUrl="http://openweathermap.org/img/wn/"+icon+"@2x.png";
+      res.write("<p>Temperature description is :"+desc+"<p>");
+      res.write("<h1>temp in "+query+" is "+temp+" degree celsius.</h1>");
+      res.write("<img src="+imgUrl+">");
+      res.send();
+    })
+  });
+})
+
+
+
+app.listen(3000,function(){
+  console.log("Server running on port 3000...");
+});
